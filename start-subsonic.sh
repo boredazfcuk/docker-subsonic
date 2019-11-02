39049fb2064cf4c538c7149909342e7171835b00
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-11-02 23:27:01 +0000

    Manual push of any changes to stack containers

--- start-subsonic.sh
+++ start-subsonic.sh
@@ -7,7 +7,7 @@ Initialise(){
    SUBSONIC_CONTEXT_PATH="/subsonic"
    SUBSONIC_PORT=4040
    SUBSONIC_HTTPS_PORT=4141
-   export SUBSONIC_HOST SUBSONIC_HOME SUBSONIC_CONTEXT_PATH SUBSONIC_DB SUBSONIC_PORT SUBSONIC_HTTPS_PORT SUBSONIC_MAX_MEMORY
+   export SUBSONIC_HOST SUBSONIC_HOME SUBSONIC_MAX_MEMORY SUBSONIC_CONTEXT_PATH SUBSONIC_DB SUBSONIC_PORT SUBSONIC_HTTPS_PORT
 
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Starting application container *****"
    if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User name not set, defaulting to 'user'"; USER="user"; fi
@@ -18,11 +18,30 @@ Initialise(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local user: ${USER}:${UID}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local group: ${GROUP}:${GID}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic directory: ${SUBSONIC_HOME}"
+
    if [ -z "${SUBSONIC_MAX_MEMORY}" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_MAX_MEMORY not set, defaulting to 512MB"
       SUBSONIC_MAX_MEMORY="512"
    else
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_MAX_MEMORY set to ${SUBSONIC_MAX_MEMORY}MB"
+   fi
+   if [ -z "${SUBSONIC_CONTEXT_PATH}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_CONTEXT_PATH not set, defaulting to /subsonic"
+      SUBSONIC_CONTEXT_PATH="/subsonic"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_CONTEXT_PATH set to ${SUBSONIC_CONTEXT_PATH}"
+   fi
+   if [ -z "${SUBSONIC_PORT}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_PORT not set, defaulting to 4040"
+      SUBSONIC_PORT=4040
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_PORT set to ${SUBSONIC_PORT}"
+   fi
+   if [ -z "${SUBSONIC_HTTPS_PORT}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_HTTPS_PORT not set, defaulting to 4141"
+      SUBSONIC_HTTPS_PORT=4141
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_HTTPS_PORT set to ${SUBSONIC_HTTPS_PORT}"
    fi
 
    if [ ! -z "${SUBSONIC_DEFAULT_MUSIC_FOLDER}" ]; then 
@@ -38,13 +57,19 @@ Initialise(){
    fi
 
    if [ ! -f "${CONFIGDIR}/https" ]; then mkdir -p "${CONFIGDIR}/https"; fi
-   if [ ! -f "${CONFIGDIR}/https/subsonic.pkcs12" ]; then
+   if [ ! -f "${CONFIGDIR}/https/subsonic.key" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate private key for encrypting communications"
       openssl ecparam -genkey -name secp384r1 -out "${CONFIGDIR}/https/subsonic.key"
+   fi
+   if [ ! -f "${CONFIGDIR}/https/subsonic.csr" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Create certificate request"
       openssl req -new -subj "/C=NA/ST=Global/L=Global/O=Subsonic/OU=Subsonic/CN=Subsonic/" -key "${CONFIGDIR}/https/subsonic.key" -out "${CONFIGDIR}/https/subsonic.csr"
+   fi
+   if [ ! -f "${CONFIGDIR}/https/subsonic.crt" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate self-signed certificate request"
       openssl x509 -req -sha256 -days 3650 -in "${CONFIGDIR}/https/subsonic.csr" -signkey "${CONFIGDIR}/https/subsonic.key" -out "${CONFIGDIR}/https/subsonic.crt"
+   fi
+   if [ ! -f "${CONFIGDIR}/https/subsonic.pkcs12" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Convert to pkcs12 format"
       openssl pkcs12 -export -inkey "${CONFIGDIR}/https/subsonic.key" -in "${CONFIGDIR}/https/subsonic.crt" -out "${CONFIGDIR}/https/subsonic.pkcs12" -password pass:subsonic
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Create pkcs12 keystore"
@@ -53,7 +78,8 @@ Initialise(){
       zip -j "${APPBASE}/subsonic-booter-jar-with-dependencies.jar" "${APPBASE}/subsonic.keystore"
    fi
 
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: https://${SUBSONIC_HOST}:4040${SUBSONIC_CONTEXT_PATH}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: http://${SUBSONIC_HOST}:4040${SUBSONIC_CONTEXT_PATH}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: https://${SUBSONIC_HOST}:4141${SUBSONIC_CONTEXT_PATH}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuration directory: ${CONFIGDIR}"
 }
 
@@ -89,6 +115,10 @@ SetOwnerAndGroup(){
 
 LaunchSubsonic(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting Subsonic as ${USER}"
+   if [ -f "${CONFIGDIR}/db/subsonic.lck" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Lock file already exists. Previous shutdown was not clean. Removing lock file"
+      rm "${CONFIGDIR}/db/subsonic.lck"
+   fi
    su -m "${USER}" -c "${APPBASE}"'/subsonic.sh'
    tail -Fn0 "${APPBASE}/subsonic_sh.log" &
    tail -Fn0 "${APPBASE}/subsonic.log"
