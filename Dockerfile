cf5da890e98e9dd03c1aa0a9102f73de98ded9e1
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-09 23:07:38 +0000

    Should be a change log. If not, I've made so many updates I've forgotten, or too few to bother updating it

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,18 @@ FROM alpine:latest
 MAINTAINER boredazfcuk
 ARG app_dependencies="tzdata openjdk8-jre fontconfig openssl zip ffmpeg lame mariadb-client wget"
 ENV config_dir="/config" \
-   app_base_dir=/Subsonic
+   app_base_dir="/Subsonic"
 
 RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD STARTED *****" && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install application dependencies" && \
    apk add --no-cache --no-progress ${app_dependencies} && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install Subsonic" && \
-   mkdir -p "${app_base_dir}/transcode/" "${app_base_dir}/db/" "${config_dir}/db/" && \
-   touch "${config_dir}/subsonic.properties" && \
-   ln -s "${config_dir}/subsonic.properties" "${app_base_dir}/" && \
-   rm "${config_dir}/subsonic.properties" && \
+   mkdir -p "${app_base_dir}" && \
    temp_dir="$(mktemp -d)" && \
    subsonic_latest_version="$(wget -qO- https://s3-eu-west-1.amazonaws.com/subsonic-public/download/checksums-sha256.txt | grep standalone | cut -d' ' -f 3 | sort -r | head -n 1)" && \
    wget -q "https://s3-eu-west-1.amazonaws.com/subsonic-public/download/${subsonic_latest_version}" --directory-prefix="${temp_dir}" && \
    tar xzf "${temp_dir}/${subsonic_latest_version}" -C "${app_base_dir}" && \
-   rm -r "${temp_dir}" && \
-   ln -s /usr/bin/ffmpeg "${app_base_dir}/transcode/" && \
-   ln -s /usr/bin/lame "${app_base_dir}/transcode/" && \
-   mv "${app_base_dir}/db/" "${config_dir}" && \
-   ln -s "${config_dir}/db/" "${app_base_dir}/"
+   rm -r "${temp_dir}"
 
 COPY start-subsonic.sh /usr/local/bin/start-subsonic.sh
 COPY healthcheck.sh /usr/local/bin/healthcheck.sh
--- start-subsonic.sh
+++ start-subsonic.sh
@@ -3,66 +3,46 @@
 ##### Functions #####
 Initialise(){
    SUBSONIC_HOST="$(hostname -i)"
-   SUBSONIC_HOME="${app_base_dir}"
+   SUBSONIC_HOME="${app_base_dir:=/Subsonic}"
    SUBSONIC_PORT=4040
    SUBSONIC_HTTPS_PORT=4141
-   export SUBSONIC_HOST SUBSONIC_HOME SUBSONIC_MAX_MEMORY subsonic_context_path SUBSONIC_DB SUBSONIC_PORT SUBSONIC_HTTPS_PORT
-
    echo -e "\n"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Starting application container *****"
-   if [ -z "${stack_user}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User name not set, defaulting to 'stackman'"; stack_user="stackman"; fi
-   if [ -z "${stack_password}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Password not set, defaulting to 'Skibidibbydibyodadubdub'"; stack_password="Skibidibbydibyodadubdub"; fi   
-   if [ -z "${user_id}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: User ID not set, defaulting to '1000'"; user_id="1000"; fi
-   if [ -z "${group}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Group name not set, defaulting to 'group'"; group="group"; fi
-   if [ -z "${group_id}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Group ID not set, defaulting to '1000'"; group_id="1000"; fi
+   if [ ! -f "${app_base_dir}/subsonic_sh.log" ]; then touch "${app_base_dir}/subsonic_sh.log"; fi
+   if [ ! -f "${app_base_dir}/subsonic.log" ]; then touch "${app_base_dir}/subsonic.log"; fi
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Username: ${stack_user:=stackman}:${user_id:=1000}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Password: ${stack_password:=Skibidibbydibyodadubdub}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Group: ${group:=subsonic}:${group_id:=1000}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuration directory: ${config_dir:=/config}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic configuration variables:"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_HOME: ${SUBSONIC_HOME}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_MAX_MEMORY: ${SUBSONIC_MAX_MEMORY:=512}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_CONTEXT_PATH: ${SUBSONIC_CONTEXT_PATH:=/}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_PORT: ${SUBSONIC_PORT:=4040}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_HTTPS_PORT: ${SUBSONIC_HTTPS_PORT:=4141}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_DEFAULT_MUSIC_FOLDER: ${SUBSONIC_DEFAULT_MUSIC_FOLDER:=/storage/music/}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_DEFAULT_PODCAST_FOLDER: ${SUBSONIC_DEFAULT_PODCAST_FOLDER:=/storage/music/Podcast}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:       SUBSONIC_DEFAULT_PLAYLIST_FOLDER: ${SUBSONIC_DEFAULT_PLAYLIST_FOLDER:=/var/playlists/}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: http://${SUBSONIC_HOST}:4040${SUBSONIC_CONTEXT_PATH}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: https://${SUBSONIC_HOST}:4141${SUBSONIC_CONTEXT_PATH}"
+   if [ ! -d "${app_base_dir}/transcode/" ]; then mkdir "${app_base_dir}/transcode/"; fi
+   if [ -f "/usr/bin/ffmpeg" ] && [ ! -L "${app_base_dir}/transcode/ffmpeg" ]; then ln -s "/usr/bin/ffmpeg" "${app_base_dir}/transcode/"; fi
+   if [ -f "/usr/bin/lame" ] && [ ! -L "${app_base_dir}/transcode/lame" ]; then ln -s "/usr/bin/lame" "${app_base_dir}/transcode/"; fi
+   if [ ! -d "${config_dir}/db/" ]; then mkdir "${config_dir}/db/"; fi
+   if [ ! -L "${app_base_dir}/db/" ]; then ln -s "${config_dir}/db/" "${app_base_dir}/"; fi
+   if [ ! -f "${config_dir}/subsonic.properties" ]; then touch "${config_dir}/subsonic.properties"; fi
+   if [ ! -L "${app_base_dir}/subsonic.properties" ]; then ln -s "${config_dir}/subsonic.properties" "${app_base_dir}/"; fi
+}
 
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local user: ${stack_user}:${user_id}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local group: ${group}:${group_id}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic directory: ${SUBSONIC_HOME}"
-
-   if [ -z "${SUBSONIC_MAX_MEMORY}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_MAX_MEMORY not set, defaulting to 512MB"
-      SUBSONIC_MAX_MEMORY="512"
-   else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_MAX_MEMORY set to ${SUBSONIC_MAX_MEMORY}MB"
-   fi
-   if [ -z "${subsonic_context_path}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    subsonic_context_path not set, defaulting to /"
-   else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    subsonic_context_path set to ${subsonic_context_path}"
-   fi
-   if [ -z "${SUBSONIC_PORT}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_PORT not set, defaulting to 4040"
-      SUBSONIC_PORT=4040
-   else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_PORT set to ${SUBSONIC_PORT}"
-   fi
-   if [ -z "${SUBSONIC_HTTPS_PORT}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_HTTPS_PORT not set, defaulting to 4141"
-      SUBSONIC_HTTPS_PORT=4141
-   else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SUBSONIC_HTTPS_PORT set to ${SUBSONIC_HTTPS_PORT}"
-   fi
-
-   if [ "${SUBSONIC_DEFAULT_MUSIC_FOLDER}" ]; then 
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic default music directory: ${SUBSONIC_DEFAULT_MUSIC_FOLDER}"
-   fi
-
-   if [ "${SUBSONIC_DEFAULT_PODCAST_FOLDER}" ]; then 
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic default music directory: ${SUBSONIC_DEFAULT_PODCAST_FOLDER}"
-   fi
-
-   if [ "${SUBSONIC_DEFAULT_PLAYLIST_FOLDER}" ]; then 
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic default music directory: ${SUBSONIC_DEFAULT_PLAYLIST_FOLDER}"
-   fi
-
-   if [ ! -f "${config_dir}/https" ]; then
+EnableSSL(){
+   if [ ! -d "${config_dir}/https" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Initialise HTTPS"
       mkdir -p "${config_dir}/https"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate private key for encrypting communications"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate server key"
       openssl ecparam -genkey -name secp384r1 -out "${config_dir}/https/subsonic.key"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Create certificate request"
       openssl req -new -subj "/C=NA/ST=Global/L=Global/O=Subsonic/OU=Subsonic/CN=Subsonic/" -key "${config_dir}/https/subsonic.key" -out "${config_dir}/https/subsonic.csr"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate self-signed certificate request"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate certificate request"
       openssl x509 -req -sha256 -days 3650 -in "${config_dir}/https/subsonic.csr" -signkey "${config_dir}/https/subsonic.key" -out "${config_dir}/https/subsonic.crt" >/dev/null 2>&1
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Convert to pkcs12 format"
       openssl pkcs12 -export -inkey "${config_dir}/https/subsonic.key" -in "${config_dir}/https/subsonic.crt" -out "${config_dir}/https/subsonic.pkcs12" -password pass:subsonic
@@ -71,10 +51,6 @@ Initialise(){
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Import keystore to Subsonic"
       zip -j "${app_base_dir}/subsonic-booter-jar-with-dependencies.jar" "${app_base_dir}/subsonic.keystore"
    fi
-
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: http://${SUBSONIC_HOST}:4040${subsonic_context_path}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic available at: https://${SUBSONIC_HOST}:4141${subsonic_context_path}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuration directory: ${config_dir}"
 }
 
 CreateGroup(){
@@ -99,8 +75,6 @@ CreateUser(){
 
 SetOwnerAndGroup(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Correct owner and group of application files, if required"
-   if [ ! -f "${app_base_dir}/subsonic_sh.log" ]; then touch "${app_base_dir}/subsonic_sh.log"; fi
-   if [ ! -f "${app_base_dir}/subsonic.log" ]; then touch "${app_base_dir}/subsonic.log"; fi
    find -L "${app_base_dir}" ! -user "${stack_user}" -exec chown "${stack_user}" {} \;
    find -L "${app_base_dir}" ! -group "${group}" -exec chgrp "${group}" {} \;
    find "${config_dir}" ! -user "${stack_user}" -exec chown "${stack_user}" {} \;
@@ -113,13 +87,15 @@ LaunchSubsonic(){
       echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Lock file already exists. Previous shutdown was not clean. Removing lock file"
       rm "${config_dir}/db/subsonic.lck"
    fi
-   su -m "${stack_user}" -c "${app_base_dir}"'/subsonic.sh'
+   export SUBSONIC_HOST SUBSONIC_HOME SUBSONIC_MAX_MEMORY SUBSONIC_CONTEXT_PATH SUBSONIC_DB SUBSONIC_PORT SUBSONIC_HTTPS_PORT SUBSONIC_DEFAULT_MUSIC_FOLDER
+   su -m "${stack_user}" -c "${app_base_dir}/subsonic.sh"
    tail -Fn0 "${app_base_dir}/subsonic_sh.log" &
    tail -Fn0 "${app_base_dir}/subsonic.log"
 }
 
 ##### Script #####
 Initialise
+EnableSSL
 CreateGroup
 CreateUser
 SetOwnerAndGroup
