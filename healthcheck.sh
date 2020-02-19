72818c6de623e17053e306a4b3458b1fcdfcc8ad
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-19 01:28:56 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 
-if [ "$(nc -z "$(hostname -i)" 4040; echo "${?}")" -ne 0 ]; then
+if [ "$(nc -z "$(hostname -i)" 4040; echo $?)" -ne 0 ]; then
    echo "Subsonic HTTP port 4040 is not responding"
    exit 1
 fi
 
-if [ "$(nc -z "$(hostname -i)" 4141; echo "${?}")" -ne 0 ]; then
+if [ "$(nc -z "$(hostname -i)" 4141; echo $?)" -ne 0 ]; then
    echo "Subsonic HTTP port 4141 is not responding"
    exit 1
 fi
